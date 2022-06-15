7150a21b3a8f69af4fb3a4f3d312ca01fc4a1e55
niklavsk <niklavsk36@gmail.com>
2022-06-15 16:53:19 +0300

    Call NER from main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from ner.named_entity_recognizer import *
 from wiki.wikidata_interaction import *
 
 
@@ -10,3 +11,6 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv)
+
+text = "The well-known country of New Zealand is a small, resourceful nation located 1,000 miles off Australia's south east coast.  New Zealand has an impressive economy that continues to grow, a physical landscape that attracts people from around the globe, and although small, New Zealand is a respected nation for its advanced civilization and stable government.  The geography of this prestigious nation can be described through five principal categories, the physical geography,  the cultural geography, the citizens' standard of living, the government, and the nation's economy. New Zealand is located in the southern hemisphere, with an absolute location of 37 degrees south longitude to 48 degrees south longitude and 167 degrees east latitude to 177 degrees east latitude.  It is composed of two major islands named the North and South Islands, and the total land area of the nation, approximately divided equally between the two islands, is 103,470 square miles.  Surprisingly, only 2 percent of the land area is arable.  New Zealand has an abundance of natural resources, explaining why the country is so wealthy compared to other nations.  These resources include fertile grazing land, oil and gas, iron, coal, timber, and excellent fishing waters."
+print('\n' + 'Combined found entities:' + '\n' + str(find_entities(text)))
--- ner/named_entity_recognizer.py
+++ ner/named_entity_recognizer.py
@@ -8,6 +8,7 @@ import en_core_web_lg
 
 # pip install -U spacy==2.3.7
 # python -m spacy download en_core_web_sm-2.2.0 --direct
+# python -m spacy download en_core_web_lg-2.2.0 --direct
 
 nlp = en_core_web_sm.load()
 
@@ -159,7 +160,7 @@ def update_model():
     nlp.to_disk(model_dir)
     print("Saved model")
 
-def find_entities():
+def find_entities(text):
     global nlp, ner, model_dir, LABELS, TRAIN_DATA, optimizer, move_names, pipe_exceptions, other_pipes
     
     should_update_model = input("Do you want to retrain the model (0/1): ")
@@ -173,7 +174,6 @@ def find_entities():
     assert nlp.get_pipe("ner").move_names == move_names
 
     #text = input("Input text: ")
-    text = "The well-known country of New Zealand is a small, resourceful nation located 1,000 miles off Australia's south east coast.  New Zealand has an impressive economy that continues to grow, a physical landscape that attracts people from around the globe, and although small, New Zealand is a respected nation for its advanced civilization and stable government.  The geography of this prestigious nation can be described through five principal categories, the physical geography,  the cultural geography, the citizens' standard of living, the government, and the nation's economy. New Zealand is located in the southern hemisphere, with an absolute location of 37 degrees south longitude to 48 degrees south longitude and 167 degrees east latitude to 177 degrees east latitude.  It is composed of two major islands named the North and South Islands, and the total land area of the nation, approximately divided equally between the two islands, is 103,470 square miles.  Surprisingly, only 2 percent of the land area is arable.  New Zealand has an abundance of natural resources, explaining why the country is so wealthy compared to other nations.  These resources include fertile grazing land, oil and gas, iron, coal, timber, and excellent fishing waters."
 
     article = nlp(text)
     found_entities = dict([(str(x), x.label_) for x in nlp(str(article)).ents])
@@ -190,10 +190,7 @@ def find_entities():
     print('\n' + "Combining results...")
     combined_entities = original_entities
     combined_entities.update(found_entities)
-    #print('\n' + 'Combined found entities:' + '\n' + str(combined_entities))
 
     #displacy.serve(article, style="ent", options=options) #http://localhost:5000
 
     return combined_entities
-
-print('\n' + 'Combined found entities:' + '\n' + str(find_entities()))
